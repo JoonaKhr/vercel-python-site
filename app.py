1c0b1b335781fcb40bf59dfbc2e15ac11c335974
JuhoKahara <juhokaha@metropolia.fi>
2023-01-09 10:01:11 +0200

    Only finds drones within set range

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 from flask import Flask
 from lxml import etree
-import urllib.request
+import urllib.request, json
 
 app = Flask(__name__)
 
 deviceY, deviceX = 250000.0, 250000.0
-
+deviceMinimumRangeY, deviceMinimumRangeX = 150000.0, 150000.0
+deviceMaximumRangeY, deviceMaximumRangeX = 350000.0, 350000.0
 @app.route("/")
 def index():
-    
+    droneList = []
     opener = urllib.request.build_opener()
     tree = etree.parse(opener.open("http://assignments.reaktor.com/birdnest/drones"))
-    for element in tree.iter("*"):
-        print(element.text)
+    droneList = tree.findall(".//drone")
+    for drone in droneList:
+        droneX = float(drone.find("positionX").text)
+        droneY = float(drone.find("positionY").text)
+        if droneX > deviceMinimumRangeX and droneX < deviceMaximumRangeX and droneY > deviceMinimumRangeY and droneY < deviceMaximumRangeY:
+            print(f'Model: {drone.find("model").text}')
+            with urllib.request.urlopen(f'http://assignments.reaktor.com/birdnest/pilots/{drone.find("serialNumber").text}') as url:
+                data = json.load(url)
+                print(data["pilotId"])
     return ""
