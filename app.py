e3354eb81502a285a70b1c723cdecda844b145f0
Joona Kähärä <joonakhr@gmail.com>
2023-01-12 18:44:44 +0200

    Gets the drone pilot information too

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from lxml import etree
-import urllib.request, json
-from requests import Session
+import json
+from requests import Session, request
 from flask_apscheduler import APScheduler
 
 app = Flask(__name__)
@@ -10,6 +10,8 @@ s = Session()
 deviceY, deviceX = 250000.0, 250000.0
 deviceMinimumRangeY, deviceMinimumRangeX = 150000.0, 150000.0
 deviceMaximumRangeY, deviceMaximumRangeX = 350000.0, 350000.0
+droneDict = {}
+dronesDict = {}
 droneList = []
 scheduler = APScheduler()
 scheduler.api_enabled = True
@@ -19,23 +21,27 @@ scheduler.init_app(app)
 def get_drones():
     responseObj = s.get("http://assignments.reaktor.com/birdnest/drones")
     tree = etree.fromstring(responseObj.content)
+    droneList = tree.findall(".//drone")
+    for drone in droneList:
+        droneX = float(drone.find("positionX").text)
+        droneY = float(drone.find("positionY").text)
+        droneSerial = drone.find("serialNumber").text
+        if droneX > deviceMinimumRangeX and droneX < deviceMaximumRangeX and droneY > deviceMinimumRangeY and droneY < deviceMaximumRangeY:
+            with request('GET', url=f'http://assignments.reaktor.com/birdnest/pilots/{droneSerial}') as url:
+                data = json.loads(url.content)
+                droneDict = {
+                    "positionX": droneX,
+                    "positionY": droneY,
+                    "pilot name": f'{data["firstName"]} {data["lastName"]}',
+                    "email": data["email"],
+                    "phone number": data["phoneNumber"]
+                }
+                if droneSerial not in dronesDict:
+                    dronesDict.update({f'{droneSerial}': droneDict})
+    print(dronesDict)
     return responseObj
 scheduler.start()
 
 @app.route("/")
 def index():
-    droneList = []
-    tree = etree.fromstring(get_drones().content)
-    droneList = tree.findall(".//drone")
-    for drone in droneList:
-        droneX = float(drone.find("positionX").text)
-        droneY = float(drone.find("positionY").text)
-        if droneX > deviceMinimumRangeX and droneX < deviceMaximumRangeX and droneY > deviceMinimumRangeY and droneY < deviceMaximumRangeY:
-            print(f'Model: {drone.find("model").text}')
-            with urllib.request.urlopen(f'http://assignments.reaktor.com/birdnest/pilots/{drone.find("serialNumber").text}') as url:
-                data = json.load(url)
-                print(data["pilotId"])
-    return ""
-
-
-    
+    return "Hello"
